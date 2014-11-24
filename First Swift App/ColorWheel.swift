acff6c73be96fca330b076143018c297ffa0865f
Armando <flores@mtu.edu>
2014-11-24 01:03:12 -0800

    Styling App

---
+++ First Swift App/ColorWheel.swift
@@ -0,0 +1,27 @@
+//
+//  ColorWheel.swift
+//  First Swift App
+//
+//  Created by Armando Flores on 11/23/14.
+//  Copyright (c) 2014 Armando Flores. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ColorWheel {
+    
+    let colorsArray = [
+        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0),
+        UIColor(red: 222/255.0, green: 171/255.0, blue: 181/255.0, alpha: 1.0),
+        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0),
+        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0,
+        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0),
+        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0)
+    ]
+    
+    func randomColor() -> UI Color {
+        var unsignedArrayCount = UInt32(colorsArray.)
+    
+    
+}
---
+++ First Swift App/MyPlayground.playground/section-1.swift
@@ -0,0 +1,7 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var redColor = UIColor(red: 200/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1)
+
+let colorsArray = [redColor]
