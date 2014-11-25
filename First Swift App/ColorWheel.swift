e620620edcbd40f14513aeabd082018bcbb82d84
Armando <flores@mtu.edu>
2014-11-25 09:33:07 -0800

    Playing with colors! :)

--- First Swift App/ColorWheel.swift
+++ First Swift App/ColorWheel.swift
@@ -6,22 +6,22 @@
 //  Copyright (c) 2014 Armando Flores. All rights reserved.
 //
 
-import Foundation
-import UIKit
-
-struct ColorWheel {
-    
-    let colorsArray = [
-        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0),
-        UIColor(red: 222/255.0, green: 171/255.0, blue: 181/255.0, alpha: 1.0),
-        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0),
-        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0,
-        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0),
-        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0)
-    ]
-    
-    func randomColor() -> UI Color {
-        var unsignedArrayCount = UInt32(colorsArray.)
-    
-    
-}
+//import Foundation
+//import UIKit
+//
+//struct ColorWheel {
+//    
+//    let colorsArray = [
+//        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0),
+//        UIColor(red: 222/255.0, green: 171/255.0, blue: 181/255.0, alpha: 1.0),
+//        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0),
+//        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0,
+//        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0),
+//        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0)
+//    ]
+//    
+//    func randomColor() -> UI Color {
+//        var unsignedArrayCount = UInt32(colorsArray.)
+//    
+//    
+//}
